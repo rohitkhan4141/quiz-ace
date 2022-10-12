2f74c11b8efd82792f46214d5410a368baf744fb
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-10-12 09:45:25 +0600

    statistics component content added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,102 @@
-// import React, { useCallback, useState } from "react";
-// import { Pie, PieChart, Sector } from "recharts";
-// import "./Statistics.css";
+import React, { useCallback, useState } from "react";
+import { Pie, PieChart, Sector } from "recharts";
+import "./Statistics.css";
 
-// import { useLoaderData } from "react-router-dom";
-import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
-  return <h1>staristics </h1>;
+  const data = useLoaderData();
+
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      payload,
+      percent,
+      value,
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor='middle' fill={fill}>
+          {payload.name}
+        </text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+          stroke={fill}
+          fill='none'
+        />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke='none' />
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          textAnchor={textAnchor}
+          fill='#333'
+        >{`Q- ${value}`}</text>
+      </g>
+    );
+  };
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
+  return (
+    <div className='statistics'>
+      <h1>Statistics</h1>
+      <PieChart width={400} height={400}>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          data={data.data}
+          cx={200}
+          cy={200}
+          innerRadius={60}
+          outerRadius={80}
+          fill='#8884d8'
+          dataKey='total'
+          onMouseEnter={onPieEnter}
+        />
+      </PieChart>
+    </div>
+  );
 };
 
 export default Statistics;
