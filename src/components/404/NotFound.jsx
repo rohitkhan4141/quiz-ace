348490724ed4350650d530417e3982f93269134b
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-10-12 09:37:57 +0600

    not found component added

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <h1>foinni</h1>;
+  return (
+    <div className='w-50 mx-auto text-center my-5'>
+      <h1>Page is not Found</h1>
+      <Link className='btn btn-primary' to='/'>
+        Home Page
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
