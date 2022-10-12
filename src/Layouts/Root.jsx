3291813d846b4dd2dd41fb451356efeea39f7d16
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-10-12 10:21:38 +0600

    navbar component renamed and fix font awesome

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Test from "../components/Test/Test";
+import NavbarComp from "../components/Navbar/Navbar";
+
 import "./Root.css";
 
 const Root = () => {
   return (
     <div className='layout'>
       {/* <Navbar /> */}
-      <Test />
+      <NavbarComp />
       <Outlet />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Text.css";
 
-const Navbar = () => {
-  const [showMediaIcon, setShowMediaIcon] = useState(false);
+const NavbarComp = () => {
   return (
-    <div className='navbar'>
-      <div className='logo'>
-        <h2>Logo</h2>
-      </div>
-      <ul className='nav-lists'>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/blog'>Blog</Link>
-        </li>
-        <li>
-          <Link to='/statistics'>Statistics</Link>
-        </li>
-      </ul>
-      {/* <button
-        onClick={() => setShowMediaIcon(!showMediaIcon)}
-        className='hamburger'
-      >
-        btn
-      </button> */}
-    </div>
+    <Navbar bg='light' expand='lg' fixed='top'>
+      <Container>
+        <Navbar.Brand className='ps-3 fs-3 text-primary' href='#'>
+          Quiz-Ace
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className='ms-auto my-2 my-lg-0 pt-3'
+            style={{ height: "60px" }}
+            navbarScroll
+          >
+            <Link className='text-decoration-none px-4' to='/'>
+              Home
+            </Link>
+            <Link className='text-decoration-none px-4' to='/statistics'>
+              Statistics
+            </Link>
+            <Link className='text-decoration-none px-4' to='/blog'>
+              Blog
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComp;
